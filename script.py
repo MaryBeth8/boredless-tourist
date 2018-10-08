0024589a0f40160be37c8b7490670edd78ff13f1
marybeth <marybeth.mclendon@gmail.com>
2018-10-07 22:36:59 -0700

    initial commit, add script.py file

---
+++ script.py
@@ -0,0 +1,3 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "So Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
